5bd048678a7d6f67e47915d0f753cb17a0ed03ed
Nomusa Shongwe <shongwenomusa990822@gmail.com>
2026-04-13 18:40:27 +0200

    Delete sql directory

--- sql/target_distribution.sql
+++
@@ -1,9 +0,0 @@
--- Fraud vs legitimate distribution.
-
-SELECT
-    is_fraud,
-    COUNT(*) AS transaction_count,
-    ROUND(100.0 * COUNT(*) / SUM(COUNT(*)) OVER (), 4) AS percentage
-FROM transactions
-GROUP BY is_fraud
-ORDER BY is_fraud;
